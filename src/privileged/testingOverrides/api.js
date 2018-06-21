a7cf7201884f5fede2ac0dde6e21c361a56cf710
Fredrik Wollsén <fredrik@neam.se>
2018-06-21 10:05:51 +0300

    Added testingOverride api to be able to override testing flags via preferences

---
+++ src/privileged/testingOverrides/api.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/* global ExtensionAPI, Preferences */
+
+ChromeUtils.import("resource://gre/modules/Console.jsm");
+ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
+ChromeUtils.import("resource://gre/modules/Preferences.jsm");
+
+this.testingOverrides = class extends ExtensionAPI {
+  getAPI(context) {
+    const { extension } = this;
+
+    // Copied here from tree
+    function makeWidgetId(id) {
+      id = id.toLowerCase();
+      // FIXME: This allows for collisions.
+      // WebExt hasn't ever had a problem.
+      return id.replace(/[^a-z0-9_-]/g, "_");
+    }
+
+    const widgetId = makeWidgetId(extension.manifest.applications.gecko.id);
+
+    return {
+      testingOverrides: {
+        getVariationNameOverride: async function getVariationNameOverride() {
+          return (
+            Preferences.get(`extensions.${widgetId}.test.variationName`) || null
+          );
+        },
+        getFirstRunTimestampOverride: async function getFirstRunTimestampOverride() {
+          return (
+            Preferences.get(`extensions.${widgetId}.test.firstRunTimestamp`) ||
+            null
+          );
+        },
+        getExpiredOverride: async function getExpiredOverride() {
+          return Preferences.get(`extensions.${widgetId}.test.expired`) || null;
+        },
+        listPreferences: async function listPreferences() {
+          return [
+            `extensions.${widgetId}.test.variationName`,
+            `extensions.${widgetId}.test.firstRunTimestamp`,
+            `extensions.${widgetId}.test.expired`,
+          ];
+        },
+      },
+    };
+  }
+};
